951acaef20d293a2e2e5fe28b6e5dc1d58dc65eb
Nikita Kazeev <kazeevn@yandex-team.ru>
2016-03-17 20:42:28 +0100

    Fixed #2

---
+++ build_and_run_docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# You can use the script to build and run the repository in the local docker
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+docker build -t reproducible_analysis_course_demo $DIR
+docker run -it -p 0.0.0.0:8888:8888 -v "$DIR":/reproducible_analysis_course reproducible_analysis_course_demo bash --login -c "cd /reproducible_analysis_course && jupyter notebook"
