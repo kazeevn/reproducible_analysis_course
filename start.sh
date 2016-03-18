9b9852f4326ec17726d7f0044d02c45902ded3bc
Nikita Kazeev <kazeevn@yandex-team.ru>
2016-03-18 12:19:44 +0100

    Everware fix

--- start.sh
+++ start.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-source activate open-ml && cd /root/analysis && jupyter notebook --no-browser --ip=0.0.0.0
+NOTEBOOK_DIR=/root/analysis
+source activate open-ml 
+
+if [ "$JPY_API_TOKEN" != "" ] ; then
+jupyterhub-singleuser \
+  --port=8888 \
+  --ip=0.0.0.0 \
+  --user=$JPY_USER \
+  --cookie-name=$JPY_COOKIE_NAME \
+  --base-url=$JPY_BASE_URL \
+  --hub-prefix=$JPY_HUB_PREFIX \
+  --hub-api-url=$JPY_HUB_API_URL \
+  --notebook-dir=$NOTEBOOK_DIR
+else
+jupyter notebook --no-browser --ip=0.0.0.0 --notebook-dir=$NOTEBOOK_DIR
+fi
