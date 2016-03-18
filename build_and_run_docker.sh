8d09d4976d8500d6694161ec9cc3579797584370
Nikita Kazeev <kazeevn@yandex-team.ru>
2016-03-18 13:57:41 +0100

    Fixed for Everware using py 2

--- build_and_run_docker.sh
+++ build_and_run_docker.sh
@@ -4,4 +4,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker build -t reproducible_analysis_course_demo $DIR
-docker run -it -p 0.0.0.0:8888:8888 -v "$DIR":/reproducible_analysis_course reproducible_analysis_course_demo bash --login -c "cd /reproducible_analysis_course && jupyter notebook"
+docker run -it -p 127.0.0.1:8888:8888 -v "$DIR":/reproducible_analysis_course reproducible_analysis_course_demo bash --login -c "cd /reproducible_analysis_course && jupyter notebook"
